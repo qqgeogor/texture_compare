533be17e5dd346d853c5ea4a58a4df6e10995660
qq <308456341@qq.com>
2015-04-14 09:36:20 +0800

    fisrt commit

---
+++ com/qqgeogor/theano/numpy_tutorial.py
@@ -0,0 +1,5 @@
+'''
+Created on 2015-4-13
+
+@author: qq
+'''
---
+++ com/qqgeogor/theano/tutorial.py
@@ -0,0 +1,8 @@
+'''
+Created on 2015-4-13
+
+@author: qq
+'''
+from theano import *    
+#Several of the symbols you will need to use are in the tensor subpackage of Theano. Let us import that subpackage under a handy name like T (the tutorials will frequently use this convention).
+from theano.tensor import T
